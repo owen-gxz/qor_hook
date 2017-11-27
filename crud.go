a571e7e5943bba84a833b2e89e47868443b98206
owen.gao <owen.gao@earlydata.com>
2017-11-27 17:36:07 +0800

    update replace resource

--- crud.go
+++ crud.go
@@ -48,34 +48,6 @@ func (f *Hook) newFindOneHandler(res *admin.Resource) func(result interface{}, m
 	return func(result interface{}, metaValues *resource.MetaValues, context *qor.Context) error {
 		// FIXME model name == resource.name(Plural)
 		tableName := inflection.Plural(strings.ToLower(res.Name))
-
-		//
-		//if res.HasPermission(roles.Read, context) {
-		//	var (
-		//		primaryQuerySQL string
-		//		primaryParams   []interface{}
-		//	)
-		//
-		//	if metaValues == nil {
-		//		primaryQuerySQL, primaryParams = res.ToPrimaryQueryParams(context.ResourceID, context)
-		//	} else {
-		//		primaryQuerySQL, primaryParams = res.ToPrimaryQueryParamsFromMetaValue(metaValues, context)
-		//	}
-		//
-		//	if primaryQuerySQL != "" {
-		//		if metaValues != nil {
-		//			if destroy := metaValues.Get("_destroy"); destroy != nil {
-		//				if fmt.Sprint(destroy.Value) != "0" && res.HasPermission(roles.Delete, context) {
-		//					context.GetDB().Table(tableName).Where(append([]interface{}{primaryQuerySQL}, primaryParams...)).Delete(result)
-		//					return ErrProcessorSkipLeft
-		//				}
-		//			}
-		//		}
-		//		return context.GetDB().Table(tableName).Where(append([]interface{}{primaryQuerySQL}, primaryParams...)).First(result).Error
-		//	}
-		//	return errors.New("failed to find")
-		//}
-		//return roles.ErrPermissionDenied
 		re := f.ResourceMap[res.Name]
 		context.DB = context.DB.Table(tableName)
 		context.Config.DB = context.DB.Table(tableName)
